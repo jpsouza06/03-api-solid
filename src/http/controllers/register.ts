4cb990e1227ec65d27d17618f6e99915c4748ed5
Joao Paulo <joao.souza@atsinformatica.com.br>
2023-03-17 16:29:25 -0300

    Caso de uso e design patterns

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,36 +1,35 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository"
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error"
+import { RegisterUseCase } from "@/use-cases/register"
 import { FastifyRequest, FastifyReply } from "fastify"
-import { hash } from "bcryptjs"
-import { prisma } from "@/lib/prisma"
 import { z } from "zod"
 
 export async function register (request: FastifyRequest, reply: FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
-    })
+   const registerBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+      password: z.string().min(6),
+   })
 
-    const { name, email, password } = registerBodySchema.parse(request.body)
+   const { name, email, password } = registerBodySchema.parse(request.body)
 
-    const password_hash = await hash(password, 6)
+   try {
+      const usersRepository = new PrismaUsersRepository()
+      const registerUseCase = new RegisterUseCase(
+         usersRepository
+      )
+      await registerUseCase.execute({
+         name,
+         email,
+         password
+      })
+   } catch (error) {
+      if(error instanceof UserAlreadyExistsError) {
+         return reply.status(409).send({ message: error.message })
+      }
+      
+      throw error
+   }
 
-    const userWithSameEmail = await prisma.user.findUnique({
-        where: {
-            email,
-        },
-    })
-
-    if (userWithSameEmail) {
-        return reply.status(409).send()
-    }
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash,
-        }
-    })
-
-    return reply.status(201).send()
+   return reply.status(201).send()
 }
