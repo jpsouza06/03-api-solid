99ff9bb066f5c270369e320a28780b6348394d27
João Paulo <joaopaulosc06@gmail.com>
2023-03-29 22:03:07 -0300

    Controllers & Testes E2E

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -13,9 +13,19 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
 
    try {
       const authenticateUseCase = makeAuthenticateUseCase()
-      await authenticateUseCase.execute({
+      const {user} = await authenticateUseCase.execute({
          email,
          password
+      })
+
+      const token = await reply.jwtSign({}, {
+         sign: {
+            sub: user.id
+         }
+      })
+
+      return reply.status(200).send({
+         token,
       })
    } catch (error) {
       if(error instanceof InvalidCredentialsError) {
@@ -24,6 +34,4 @@ export async function authenticate(request: FastifyRequest, reply: FastifyReply)
       
       throw error
    }
-
-   return reply.status(200).send()
 }
