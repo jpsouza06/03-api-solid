ff192984e7e3bcc423d612ab1a09bcda054f4a3f
Joao Paulo <joaopaulosc06@gmail.com>
2023-04-03 14:16:54 -0300

    Refresh Token & RBAC

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from "./authenticate"
 import { profile } from "./profile"
 import { register } from "./register"
 import { verifyJwt } from "../../middlewares/verify-jwt"
+import { refresh } from "./refresh"
 
 export async function usersRoutes(app: FastifyInstance) {
    app.post("/users", register)
    app.post("/sessions", authenticate)
+
+   app.patch("/token/refresh", refresh)
 
    /**Authenticad */
    app.get("/me", { onRequest: [verifyJwt]}, profile)
